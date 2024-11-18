3c6216e87abbbcf4de3438180eb20cdf656a0d63
Luana Lima <luanalima.2006@hotmail.com>
2024-11-18 11:25:25 -0300

    feat: add and style header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,43 @@
 import S from "./header.module.scss";
+import logo from "../../assets/logo.png";
+import busca from "../../assets/busca.png";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Inicio from "../../Pages/Inicio/Inicio";
+import Doados from "../../Pages/Doados/Doados";
+import QueroDoar from "../../Pages/QueroDoar/QueroDoar";
 // importar a logo do assets
 
 export default function Header() {
   return (
-    <header>
-      <section>{/* logo */}</section>
-      <nav>
-        <ul>
-          <li>Inicio</li>
-          <li>Livros Doados</li>
-          <li>Quero doar</li>
-        </ul>
-      </nav>
+    <BrowserRouter>
+      <header>
+        <section className={S.boxLogo}>
+          <img src={logo} alt="imagem de um livro" />
+          <h1>Livros Vai na Web</h1>
+        </section>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/doados">Livros Doados</Link>
+            </li>
+            <li>
+              <Link to="/queroDoar">Quero Doar</Link>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <input type="text" placeholder="O que você procura?" />
+        </div>
+      </header>
 
-      <input type="text" placeholder="O que você procura?" />
-    </header>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/doados" element={<Doados />} />
+        <Route path="/queroDoar" element={<QueroDoar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
