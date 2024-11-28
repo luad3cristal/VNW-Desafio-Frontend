7b28d591c2ccfeb1c9ad26f5cd53b5cfa3cb8d3d
Luana Lima <luanalima.2006@hotmail.com>
2024-11-28 14:24:13 -0300

    feat: add and style quero doar

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,9 +1,32 @@
 import S from "./QueroDoar.module.scss";
+import livroInfo from "../../assets/livro-info.png";
 
 export default function QueroDoar() {
   return (
-    <section>
-      <h2>Quero Doar</h2>
-    </section>
+    <main>
+      <section className={S.doacoesBox}>
+        <div className={S.aviso}>
+          <p>
+            Por favor, preencha o formulário com suas informações e as
+            informações do Livro
+          </p>
+        </div>
+        <form>
+          <div className={S.titulo}>
+            <img src={livroInfo} alt="" />
+            <h2>Informações do Livro</h2>
+          </div>
+
+          <div className={S.inputBox}>
+            <input type="text" placeholder="Título" />
+            <input type="text" placeholder="Categoria" />
+            <input type="text" placeholder="Autor" />
+            <input type="url" placeholder="Link da Imagem" />
+
+            <input type="submit" placeholder="Doar" />
+          </div>
+        </form>
+      </section>
+    </main>
   );
 }
