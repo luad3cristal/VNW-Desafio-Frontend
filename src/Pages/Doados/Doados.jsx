e2c0a8e314b93650d45e0f47912d3d5747681044
Luana Lima <luanalima.2006@hotmail.com>
2025-01-28 12:31:18 -0300

    refactor: remover comentários do código

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -19,22 +19,6 @@ export default function Doados() {
           </div>
         </section>
       </article>
-
-      {/* <section className={S.doadosBox}>
-
-        <article className={S.cards}>
-          <div className={S.card}>
-            <div>
-              <img src={livroExemplo} alt="capa do livro Protagonista" />
-            </div>
-            <div>
-              <p className={S.titulo}>O protagonista</p>
-              <p className={S.autor}>Susanne Andrade</p>
-              <p className={S.estilo}>Ficção</p>
-            </div>
-          </div>
-        </article>
-      </section> */}
     </main>
   );
 }
