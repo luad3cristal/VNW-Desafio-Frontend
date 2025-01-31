ae8af6ec1a403b6c5c0e69a09e99a1b1a2553c69
Luana Lima <luanalima.2006@hotmail.com>
2025-01-30 23:00:12 -0300

    refactor: reorganizar o conteúdo da página início

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -6,45 +6,41 @@ import balanca from "../../assets/balanca.png";
 
 export default function Inicio() {
   return (
-    <main>
-      <section className={S.inicioBox}>
-        <section className={S.announcement}>
-          <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
-        </section>
-        <section className={S.content}>
-          <div>
-            <h2>Por que devo doar?</h2>
-          </div>
-
-          <section className={S.cards}>
-            <div className={S.card}>
-              <img src={grupo} alt="" />
-              <p>
-                Oferece livros a quem não tem acesso, ajudando a reduzir a
-                exclusão social.
-              </p>
-            </div>
-            <div className={S.card}>
-              <img src={leitura} alt="" />
-              <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
-            </div>
-            <div className={S.card}>
-              <img src={conhecimento} alt="" />
-              <p>
-                Fornece conhecimento e inspiração, permitindo que indivíduos
-                transformem suas vidas.
-              </p>
-            </div>
-            <div className={S.card}>
-              <img src={balanca} alt="" />
-              <p>
-                Garante que todos, independentemente de sua condição, tenham
-                oportunidades de aprendizado.
-              </p>
-            </div>
-          </section>
-        </section>
+    <main className={S.inicio_container}>
+      <section className={S.announcement}>
+        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
       </section>
+      <article className={S.content}>
+        <h2>Por que devo doar?</h2>
+
+        <section className={S.cards}>
+          <div className={S.card}>
+            <img src={grupo} alt="" />
+            <p>
+              Oferece livros a quem não tem acesso, ajudando a reduzir a
+              exclusão social.
+            </p>
+          </div>
+          <div className={S.card}>
+            <img src={leitura} alt="" />
+            <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+          </div>
+          <div className={S.card}>
+            <img src={conhecimento} alt="" />
+            <p>
+              Fornece conhecimento e inspiração, permitindo que indivíduos
+              transformem suas vidas.
+            </p>
+          </div>
+          <div className={S.card}>
+            <img src={balanca} alt="" />
+            <p>
+              Garante que todos, independentemente de sua condição, tenham
+              oportunidades de aprendizado.
+            </p>
+          </div>
+        </section>
+      </article>
     </main>
   );
 }
