9c2baec0b5126e03cc0782186928cf77cae79978
Luana Lima <luanalima.2006@hotmail.com>
2025-03-31 10:52:42 -0300

    feat: fetch and integrate API data

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,20 @@
 import S from "./Doados.module.scss";
 import livroExemplo from "../../assets/livro-exemplo.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Doados() {
+  const [livros, setLivros] = useState([]);
+
+  const getLivros = async () => {
+    const response = await axios.get("https://vnw-backend.onrender.com/livros");
+    setLivros(response.data);
+  };
+
+  useEffect(() => {
+    getLivros();
+  }, []);
+
   return (
     <main>
       <article className={S.livros_doados}>
@@ -17,6 +30,21 @@ export default function Doados() {
               <p>Ficção</p>
             </div>
           </div>
+          {livros.map((livro, index) => (
+            <div key={index} className={S.card}>
+              <div className={S.card_image}>
+                <img
+                  src={livro.imagem_url}
+                  alt={`capa do livro ${livro.titulo}`}
+                />
+              </div>
+              <div className={S.card_content}>
+                <p>{livro.titulo}</p>
+                <p>{livro.autor}</p>
+                <p>{livro.categoria}</p>
+              </div>
+            </div>
+          ))}
         </section>
       </article>
     </main>
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,39 @@
 import S from "./QueroDoar.module.scss";
 import livroInfo from "../../assets/livro-info.png";
+import axios from "axios";
+import { useState } from "react";
 
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [imagem_url, setImagem_url] = useState("");
+
+  const sendData = async () => {
+    const urlAPI = "https://vnw-backend.onrender.com/doar";
+
+    const dataSend = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url,
+    };
+
+    const sendAPI = await axios.post(urlAPI, dataSend);
+
+    alert("Livro enviado!");
+    
+    setTitulo("");
+    setCategoria("");
+    setAutor("");
+    setImagem_url("");
+  };
+
+  const getTitle = (e) => setTitulo(e.target.value);
+  const getCathegory = (e) => setCategoria(e.target.value);
+  const getAuthor = (e) => setAutor(e.target.value);
+  const getImage = (e) => setImagem_url(e.target.value);
+
   return (
     <main>
       <section className={S.doacoesBox}>
@@ -11,19 +43,39 @@ export default function QueroDoar() {
             informações do Livro
           </p>
         </div>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <div className={S.titulo}>
             <img src={livroInfo} alt="" />
             <h2>Informações do Livro</h2>
           </div>
 
           <div className={S.inputBox}>
-            <input type="text" placeholder="Título" />
-            <input type="text" placeholder="Categoria" />
-            <input type="text" placeholder="Autor" />
-            <input type="url" placeholder="Link da Imagem" />
+            <input
+              type="text"
+              placeholder="Título"
+              onChange={getTitle}
+              value={titulo}
+            />
+            <input
+              type="text"
+              placeholder="Categoria"
+              onChange={getCathegory}
+              value={categoria}
+            />
+            <input
+              type="text"
+              placeholder="Autor"
+              onChange={getAuthor}
+              value={autor}
+            />
+            <input
+              type="url"
+              placeholder="Link da Imagem"
+              onChange={getImage}
+              value={imagem_url}
+            />
 
-            <input type="submit" placeholder="Doar" />
+            <input type="submit" placeholder="Doar" onClick={sendData} />
           </div>
         </form>
       </section>
